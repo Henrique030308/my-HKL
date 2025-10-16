94e0fbac4f5027c101360e165baaa3e7180f42cb
henrique030308 <henrique.2910@aluno.pr.senac.br>
2025-10-16 12:27:45 -0300

    16/10/25 - 12:27h

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,13 +1,59 @@
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  TextInput,
+} from "react-native";
 import React from "react";
+import Header from "../components/Header";
+import Divisor from "../../assets/img/ou.png";
+import Google from "../../assets/img/iconGoogle.png";
+import Facebook from "../../assets/img/iconFacebook.png";
+import Telefone from "../../assets/img/telefone.png";
 
-export default function Login({ navigation }) {
+export default function Cadastro({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Login</Text>
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("TabNavigator") }>
-        <Text style={styles.buttonText}>Ir para abas</Text>
-      </TouchableOpacity>
+      <Header />
+      <View>
+        <Text style={styles.titulo}>LOGIN</Text>
+      </View>
+      <View style={styles.caixabotao}>
+        <Text style={styles.cadastro}>E-mail</Text>
+        <TextInput
+          placeholder="******@gmail.com"
+          style={styles.input}
+          keyboardType="text"
+        />
+        <Text style={styles.cadastro}>Senha</Text>
+        <TextInput
+          placeholder="******"
+          style={styles.input}
+          keyboardType="text"
+        />
+        <Text style={styles.texto}>Não lembra a senha?</Text>
+        <TouchableOpacity
+          style={styles.botao}
+          onPress={() => navigation.navigate("TabNavigator")}
+        >
+          <Text style={styles.login}>ENTRAR</Text>
+        </TouchableOpacity>
+        <Image source={Divisor} style={{ marginTop: 30, marginBottom: 30 }} />
+        <TouchableOpacity style={styles.outros}>
+          <Image source={Google} style={{ marginLeft: 15 }} />
+          <Text style={styles.meios}>Entrar com o Google</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.outros}>
+          <Image source={Facebook} style={{ marginLeft: 15 }} />
+          <Text style={styles.meios}>Entrar com o Facebook</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.outros}>
+          <Image source={Telefone} style={{ marginLeft: 15 }} />
+          <Text style={styles.meios}>Entrar com o Telefone</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -15,21 +61,75 @@ export default function Login({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    backgroundColor: "#ffffffff",
     alignItems: "center",
-    backgroundColor: "#fff",
+    justifyContent: "center",
   },
-  title: {
-    fontSize: 24,
-    marginBottom: 20,
+  titulo: {
+    color: "#fbb800",
+    fontSize: 45,
+    fontWeight: "bold",
+    paddingTop: 15,
+    paddingBottom: 35,
+    textDecorationLine: "underline",
   },
-  button: {
-    backgroundColor: "#005594",
-    padding: 15,
-    borderRadius: 8,
+  caixabotao: {
+    flex: 4,
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+    textAlign: "left",
+    backgroundColor: "#ffffff",
   },
-  buttonText: {
+  cadastro: {
+    color: "#000000",
+    fontSize: 25,
+    paddingBottom: 10,
+    paddingTop: 15,
+    alignSelf: "flex-start",
+    marginLeft: 30,
+  },
+  input: {
+    width: 360,
+    height: 50,
+    borderRadius: 10,
+    borderWidth: 3,
+    backgroundColor: "#fbe8ba",
+    borderColor: "#fab802",
+  },
+  botao: {
+    marginTop: 40,
+    backgroundColor: "#fbb800",
+    height: 50,
+    width: 350,
+    borderRadius: 16,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  login: {
     color: "#fff",
-    fontSize: 18,
+    fontSize: 35,
+    fontWeight: "bold",
+  },
+  texto: {
+    alignSelf: "start",
+    color: "#fbb800",
+    fontSize: 16,
+    marginLeft: 30,
+  },
+  outros: {
+    flexDirection: "row",
+    width: 350,
+    height: 50,
+    borderRadius: 50,
+    alignItems: "center",
+    borderWidth: 3,
+    backgroundColor: "#fbe8ba",
+    borderColor: "#fab802",
+    marginTop: 15,
+  },
+  meios: {
+    fontSize: 23,
+    marginLeft: 10,
   },
 });
