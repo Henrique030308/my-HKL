a4211266eb921d357eee8b4aa88be3082eff7820
Henrique <125399449+Henrique030308@users.noreply.github.com>
2025-10-23 16:14:15 -0300

    23/10 16:14

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,8 @@
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 import Header from "../components/Header";
+import Ondinha from "../../assets/img/ondinha.png"
+import Sesc from '../../assets/img/cafeSesc.png';
+import Senac from '../../assets/img/cafeSenac.png';
 
 export default function Home() {
   return (
@@ -7,15 +10,23 @@ export default function Home() {
       <Header />
       <View style={styles.cima}>
         <Text style={styles.titulo}>SEU SALDO:</Text>
-        <Text style={styles.valor}>R$00,00</Text>
+        <Text style={styles.valor}>R$0,00</Text>
         <TouchableOpacity
           style={styles.botao}
           onPress={() => navigation.navigate("TabNavigator")}
         >
           <Text style={styles.transferir}>TRANSFERIR</Text>
         </TouchableOpacity>
+        <Image source={Ondinha} style={styles.ondinha}/>
       </View>
-      <View style={styles.baixo}></View>
+      <View style={styles.baixo}>
+        <TouchableOpacity>
+          <Image source={Sesc} style={styles.cafe}/>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image source={Senac} style={styles.cafe}/>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -36,7 +47,7 @@ const styles = StyleSheet.create({
     color: "#fbb800",
     fontSize: 45,
     fontWeight: "bold",
-    paddingTop: 15,
+    paddingTop: 40,
     paddingBottom: 35,
     textDecorationLine: "underline",
   },
@@ -60,8 +71,27 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
 
+  // ...existing code...
+
+  ondinha: {
+    marginTop: 30,
+    width: '100%', // Ocupa 100% da largura da tela
+    height: 50, // Altura fixa para a onda
+    resizeMode: 'stretch', // Estica a imagem para preencher o espaço
+  },
+
+// ...existing code...
+
+
   baixo: {
+    justifyContent: "center",
+    alignItems: "center",
     height: "50%",
     backgroundColor: "#095593",
   },
+  cafe:{
+    width: 280,
+    resizeMode: 'contain',
+    marginVertical: 15,
+  }
 });
