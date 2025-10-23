ea73f99ecf94fd6b596afb46e97bfac0a930fe27
Henrique <125399449+Henrique030308@users.noreply.github.com>
2025-10-23 15:54:08 -0300

    vava

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import Header from "../components/Header";
-=======
-import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
-import Header from "../components/Header";
-import Ondinha from "../../assets/img/ondinha.png"
-import Sesc from '../../assets/img/cafeSesc.png';
-import Senac from '../../assets/img/cafeSenac.png';
->>>>>>> 2b345c3c2f438e3d3813faf5d4cbe6e65a3478c2
 
 export default function Home() {
   return (
@@ -15,32 +7,15 @@ export default function Home() {
       <Header />
       <View style={styles.cima}>
         <Text style={styles.titulo}>SEU SALDO:</Text>
-<<<<<<< HEAD
         <Text style={styles.valor}>R$00,00</Text>
-=======
-        <Text style={styles.valor}>R$0,00</Text>
->>>>>>> 2b345c3c2f438e3d3813faf5d4cbe6e65a3478c2
         <TouchableOpacity
           style={styles.botao}
           onPress={() => navigation.navigate("TabNavigator")}
         >
           <Text style={styles.transferir}>TRANSFERIR</Text>
         </TouchableOpacity>
-<<<<<<< HEAD
       </View>
       <View style={styles.baixo}></View>
-=======
-        <Image source={Ondinha} style={styles.ondinha}/>
-      </View>
-      <View style={styles.baixo}>
-        <TouchableOpacity>
-          <Image source={Sesc} style={styles.cafe}/>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Image source={Senac} style={styles.cafe}/>
-        </TouchableOpacity>
-      </View>
->>>>>>> 2b345c3c2f438e3d3813faf5d4cbe6e65a3478c2
     </View>
   );
 }
@@ -61,11 +36,7 @@ const styles = StyleSheet.create({
     color: "#fbb800",
     fontSize: 45,
     fontWeight: "bold",
-<<<<<<< HEAD
     paddingTop: 15,
-=======
-    paddingTop: 40,
->>>>>>> 2b345c3c2f438e3d3813faf5d4cbe6e65a3478c2
     paddingBottom: 35,
     textDecorationLine: "underline",
   },
@@ -88,27 +59,9 @@ const styles = StyleSheet.create({
     fontSize: 35,
     fontWeight: "bold",
   },
-<<<<<<< HEAD
 
   baixo: {
     height: "50%",
     backgroundColor: "#095593",
   },
-=======
-  ondinha: {
-    marginTop: 30,
-  },
-
-  baixo: {
-    justifyContent: "center",
-    alignItems: "center",
-    height: "50%",
-    backgroundColor: "#095593",
-  },
-  cafe:{
-    width: 280,
-    resizeMode: 'contain',
-    marginVertical: 15,
-  }
->>>>>>> 2b345c3c2f438e3d3813faf5d4cbe6e65a3478c2
 });
